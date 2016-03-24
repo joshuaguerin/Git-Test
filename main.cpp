7006bc411a1e61eea00f12010e42cca7fea45f75
Joshua T. Guerin <jguerin@utm.edu>
2016-03-24 08:45:20 -0500

    Adding sum function, user interaction.

--- main.cpp
+++ main.cpp
@@ -3,8 +3,24 @@
 
 using namespace std;
 
+int sum(int n);
+ 
 int main() {
-  cout << "Hello World!" << endl;
-
+  int n=0;
+  
+  cout << "Please enter a value: ";
+  cin >> n;
+  
+  cout << "Total from 1 to " << n << " is: " << sum(n) << endl;
+  
   return 0;
+}
+
+int sum(int n) {
+  int sum=0;
+  
+  for(int i=1; i<=n; i++)
+    sum += i;
+  
+  return sum;
 }
